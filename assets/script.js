20afd9538104e5f72105bb4747668688eb11ec39
Mariana <marianabraz.souza01@gmail.com>
2023-02-19 16:38:41 -0300

    caracteres especiais

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,9 @@ let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
 let containerPassword = document.querySelector("#container-password");
 let tooltip = document.querySelector("#tooltip");
+var especiale = document.getElementById("especiale");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let charset = "";
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
@@ -13,12 +14,24 @@ slider.oninput = function(){
     sizePassword.innerHTML = this.value;
 }
 
+
+function especial(){
+    if (especiale.checked == true){
+         charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#&*$";
+         console.log(charset);         
+    } else {
+         charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+         console.log(charset);
+        }
+}
+
+
 function generetePassword(){
     let pass = "";
     for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
         pass += charset.charAt(Math.floor(Math.random() * n));
     }
-    
+
     containerPassword.classList.remove("hide");
     password.innerHTML = pass;
     novaSenha = pass;
@@ -28,7 +41,5 @@ function copyPassword(){
     navigator.clipboard.writeText(novaSenha);
 }
 
-if (password.clicked == true){
-    alert("aa")
-}
-//quando clicar em password mudar o 'clique para copiar a senha' para 'senha copiada'
+
+//quando clicar em password mudar o 'clique para copiar a senha' para 'senha copiada' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
